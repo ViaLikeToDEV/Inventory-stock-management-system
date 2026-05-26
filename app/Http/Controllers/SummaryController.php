5ab40625da538c06123a6f90d582d505e6628e3c
Ratrawe Lha-ong <vijvijkk82@gmail.com>
2026-05-27 02:13:19 +0700

    Summary Request Optimization
    make it asynchronous

--- app/Http/Controllers/SummaryController.php
+++ app/Http/Controllers/SummaryController.php
@@ -3,20 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http; // <-- ขาดตัวนี้!
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\Pool;
 
 class SummaryController extends Controller
 {
-    public function getDailySummary(){
-        $tiktokshopRes = Http::post('https://script.google.com/macros/s/AKfycbyi-Pz8el_aChRSp8bqxubT-8tXsEiTsk4P_ZF88r26mSPAnIO2vtvRTcOLGXgUzcxO/exec');
-        $shopeeRes = Http::post('https://script.google.com/macros/s/AKfycbzL9eu8Z-JmerV7k8j2zqr2H97imIj46xNIr1YchAESkv9LkZqQS_LTMEc_0m8umaTf/exec', ['action' => 'total']);
+    public function getDailySummary()
+    {
+        $responses = Http::pool(fn (Pool $pool) => [
+        $pool->as('tiktokshop')->post(config('services.tiktokshop_script_url')),
+        $pool->as('shopee')->post(config('services.shopee_script_url'), ['action' => 'total']),
+    ]);
 
-        $result = [
-            'TiktokShop' => $tiktokshopRes->json(),
-            'Shopee' => $shopeeRes->json(),
-        ];
+    $result = [
+        'TiktokShop' => $responses['tiktokshop']->json(),
+        'Shopee'     => $responses['shopee']->json(),
+    ];
 
-        // แปลง Response ของ Google เป็น JSON Array แบบสวยๆ ให้ React เอาไปใช้ง่าย
-        return response()->json($result);
-    }
+    return response()->json($result);
+        }
 }
